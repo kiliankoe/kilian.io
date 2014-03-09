4e1fe133efa84049ef6d641d952c8e721b8965ad
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-03-09 12:35:38 +0100

    local content is now inline and animated by jquery

---
+++ js/animation.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	// Slide local content like aboutme etc. on click events.
+	$(".inlinelink").on("click", function(){
+		$(this).closest("li").find(".nodisplay").slideToggle();
+	});
+});
