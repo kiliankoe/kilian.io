ce6b2fb35f1df6e43cf80b021513f788bab0d0e1
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-03-15 09:47:46 +0100

    fixed jump to top when jquery animation was called in a scrolled state

--- js/animation.js
+++ js/animation.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 	// Slide local content like aboutme etc. on click events.
 	$(".inlinelink").on("click", function(){
 		$(this).closest("li").find(".nodisplay").slideToggle('fast');
+		return false;
 	});
 });
