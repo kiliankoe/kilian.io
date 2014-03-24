4c8c6caa6de820e8e5318f114643fb3ba92f81d1
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-03-24 12:23:27 +0100

    is it easter already?

--- js/stuff.js
+++ js/stuff.js
@@ -7,4 +7,21 @@ $(document).ready(function(){
 });
 
 // konami code!
-cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () { $('span#name').text("Kili-chan") });
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+	function getRandomColor() {
+	    var letters = '0123456789ABCDEF'.split('');
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.round(Math.random() * 15)];
+	    }
+	    return color;
+	}
+	$('body').css('background-color',getRandomColor());
+	$('.header').css('color',getRandomColor());
+	$('div').css('font-family','Comic Sans MS');
+});
+
+// konami fail!
+cheet('↑ ↑ ↓ ↓ ← → ← → a b', function(){
+	alert("Yeah... Try again.");
+});
